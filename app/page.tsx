5c352adb9d0f20da47765e390b786394472a8601
Epiphanus <onyesoepiphanus@gmail.com>
2025-01-14 09:24:24 +0100

    Added a description to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { Heart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex-center mx-auto h-screen">
-      <div className="flex-center flex-col">
+    <div className="flex-center flex-col mx-auto min-h-screen gap-12">
+      <div className="flex-center flex-col h-[70vh]">
         <Link
           href="/"
           className="flex gap- items-center cursor-pointer xl:hidden"
@@ -31,6 +32,46 @@ const Home = () => {
             Get Started
           </Link>
         </div>
+      </div>
+
+      <div className="flex-center flex-col w-[30rem] md:w-[45rem] gap-6 mb-4">
+        <h2 className="header">How to use the application!!</h2>
+
+        <p className="sub-header">
+          If this is your first visit and you would like to get a thorough view
+          of application, please follow these steps
+        </p>
+
+        <ul className="list-item list-outside list-disc space-y-4 tracking-wide leading-relaxed">
+          <li>
+            In the login page, you'll find a link with the text, 'Admin'. Click
+            on it
+          </li>
+          <li>
+            You can type in my email address{" "}
+            <i>
+              <em>onyesoepiphanus@gmail.com</em>
+            </i>{" "}
+            and quickly write an email to me requesting for the code. I'll send
+            that to you in a jiffy
+          </li>
+          <li>
+            Or you can use the global password, 123456, if you're too busy
+          </li>
+          <li>
+            Create a doctor or a patient account. Log out and use the details
+            you provided to log in using the normal sign in page
+          </li>
+
+          <li>
+            Have fun navigating through the platform and if you get stuck, don't
+            hesitate to reach out. <br />
+            <span className="flex gap-2">
+              Thank you for being here{" "}
+              <Heart className="text-red-700 fill-red-700" />
+            </span>
+          </li>
+        </ul>
       </div>
     </div>
   );
